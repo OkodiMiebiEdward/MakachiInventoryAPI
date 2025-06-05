0698726b794ff55a1d6f37ed699d3644f60ce0e2
Miebi <User@DESKTOP-1EK6ASL>
2025-06-05 09:18:04 +0100

    Added endpoint to delete role

--- WebServiceMakachi/InventoryAPI/Controllers/AuthenticationController.cs
+++ WebServiceMakachi/InventoryAPI/Controllers/AuthenticationController.cs
@@ -62,7 +62,7 @@ public class AuthenticationController : ControllerBase
             _config.GetValue<string>("Authentication:Audience"),
             claims,
             DateTime.UtcNow, // When this token becomes valid
-            DateTime.UtcNow.AddMinutes(1), // When the token will expire
+            DateTime.UtcNow.AddMinutes(20), // When the token will expire
             signingCredentials);
 
         return new JwtSecurityTokenHandler().WriteToken(token);
