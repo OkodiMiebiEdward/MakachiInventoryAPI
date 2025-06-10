29b9944d98cea3b680180be65c02df0b4d14eef3
Miebi <User@DESKTOP-1EK6ASL>
2025-06-10 12:35:47 +0100

    Chore: Created product, Category and Variant model

---
+++ WebServiceMakachi/InventoryAPI/Model/Variant.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryAPI.Model;
+
+public class Variant
+{
+    public int Id { get; set; } // Primary Key
+    public string Name { get; set; } // e.g., Color, Size, Volume
+    public string Value { get; set; } // e.g., Red, Large, 500ml
+    public int ProductId { get; set; } // Foreign Key linking to Product
+    public Product Product { get; set; }
+}
