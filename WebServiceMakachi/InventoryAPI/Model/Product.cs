e569b3683f882f8aa3dba145ab406d7a447e17e1
Miebi <User@DESKTOP-1EK6ASL>
2025-06-25 05:18:45 +0100

    Feat:Added categoryId to the model

--- WebServiceMakachi/InventoryAPI/Model/Product.cs
+++ WebServiceMakachi/InventoryAPI/Model/Product.cs
@@ -5,6 +5,7 @@ public class Product
     public int Id { get; set; }
     public string ProductName { get; set; }
     public string? ProductDescription { get; set; }
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
     public string SKU { get; set; }
     public string BarCodeNumber { get; set; }
