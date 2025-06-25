2347c4d9fc00fd7c93e9cd7936e1ae73cc9fe88f
Miebi <User@DESKTOP-1EK6ASL>
2025-06-25 05:17:50 +0100

    Chore:Added new controller for stock management

---
+++ WebServiceMakachi/InventoryAPI/Controllers/StockController.cs
@@ -0,0 +1,23 @@
+﻿using InventoryAPI.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly InventoryDbContext _inventoryDbContext;
+
+        public StockController(IConfiguration config,
+            InventoryDbContext inventoryDbContext)
+        {
+            _config = config;
+            _inventoryDbContext = inventoryDbContext;
+        }
+
+
+    }
+}
